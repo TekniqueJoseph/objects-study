352684a6912a067ab333e55c1d7ffeb3ec5a3ac1
TekniqueJoseph <joejacksun@gmail.com>
2023-04-17 19:00:17 -0400

    added more styles

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,25 +1,35 @@
 
+// const blogs = [
+//     {title: 'Good Voice', likes: 30},
+//     {title: 'Smooth Flow', likes: 50}
+// ]
+
+// console.log(blogs)
+
 let user = {
     name: 'Butterfly',
     age: 50,
     email: 'mecca@thebronx.com',
     location: 'Kemet',
-    blogs:['Good Voice', 'Smooth Flow'],
-    phrase1: function() {
+    blogs:[
+        {title: 'Good Voice', likes: 30},
+        {title: 'Smooth Flow', likes: 50}
+    ],
+    phrase1() {
         console.log('68 inches above sea level');
     },
-    phrase2: function() {
+    phrase2() {
         console.log('93 million miles above these devils')
     },
-    logBlog: function(){
+    logBlogs(){
         // console.log(this.blogs[1])
         console.log('This emcee is dope.')
         this.blogs.map(blog => {
-            console.log(blog)
+            console.log(blog.title, blog.likes)
         })
     } 
 }
-console.log(user)
+// console.log(user)
 // console.log(user.name)
 // console.log(user.age)
 // console.log(user['location'])
@@ -30,11 +40,19 @@ console.log(user)
 // console.log(Object.values(user))
 // console.log(user.blog[0])
 
-user.phrase1()
-user.phrase2()
-user.logBlog()
+// user.phrase1()
+// user.phrase2()
+// user.logBlogs()
 
-// setTimeout (() => {
-//     console.log('Waited 5 Seconds')
-// }, 5000);
+// Math Object
+console.log(Math)
+console.log(Math.PI)
+console.log(Math.E)
 
+const area = 7.7;
+console.log(Math.round(area))
+console.log(Math.floor(area))
+console.log(Math.ceil(area))
+console.log(Math.trunc(area))
+
+// Random numbers
