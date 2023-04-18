5bddae3bf094cc69b6bc0e1b0c368f9e241cb183
TekniqueJoseph <joejacksun@gmail.com>
2023-04-17 20:03:21 -0400

    added primative vs reference lesson

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -45,15 +45,39 @@ let user = {
 // user.logBlogs()
 
 // Math Object
-console.log(Math)
-console.log(Math.PI)
-console.log(Math.E)
+// console.log(Math)
+// console.log(Math.PI)
+// console.log(Math.E)
 
 const area = 7.7;
-console.log(Math.round(area))
-console.log(Math.floor(area))
-console.log(Math.ceil(area))
-console.log(Math.trunc(area))
-console.log(Math.floor(area))
+// console.log(Math.round(area))
+// console.log(Math.floor(area))
+// console.log(Math.ceil(area))
+// console.log(Math.trunc(area))
 
 // Random numbers
+const random = Math.random()
+
+// console.log(random)
+// console.log(Math.round(random * 100))
+
+// primitive values
+
+let scoreOne = 50;
+let scoreTwo = scoreOne
+
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`)
+
+scoreOne = 100;
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`)
+
+// reference types
+
+const userOne = {name: 'Jazzy', age: 50}
+const userTwo = userOne;
+
+console.log(userOne, userTwo)
+
+userOne.age = 40;
+
+console.log(userOne, userTwo)
